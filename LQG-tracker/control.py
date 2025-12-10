23026e4e055fbc4505e3965cef9ea2e7ef101838
maxlchien <90492587+maxlchien@users.noreply.github.com>
2025-12-10 14:24:39 -0500

    Implement LQG tracking control

--- LQG-tracker/control.py
+++ LQG-tracker/control.py
@@ -4,13 +4,18 @@ from dynamics import NoisyDoubleIntegrator
 from estimator import KalmanFilter
 
 
-class LQGController:
+class LQGTracker:
     system: NoisyDoubleIntegrator
     estimator: KalmanFilter
 
     M: np.ndarray
     Q: np.ndarray
     R: np.ndarray
+
+    K_series: np.ndarray
+    Kov_series = np.ndarray
+
+    r_series: np.ndarray
 
     num_steps: int
 
@@ -21,7 +26,8 @@ class LQGController:
         gamma: float,
         system: NoisyDoubleIntegrator,
         estimator: KalmanFilter,
-        num_steps: int
+        reference_trajectory: np.ndarray,
+        num_steps: int,
     ):
         self.system = system
         self.estimator = estimator
@@ -29,9 +35,86 @@ class LQGController:
         self.M = np.diag([alpha, beta])
         self.Q = self.M
         self.R = gamma
-        self.num_setps = num_steps
 
-    def step(self):
-        # estimate the system state
+        self.r_series = reference_trajectory
+
+        self.num_steps = num_steps
+
+        self.initialize_gain_matrices()
+
+    def _riccati(self, P: np.ndarray) -> np.ndarray:
+        return (
+            self.M
+            + self.system.F.T @ P @ self.system.F
+            - (self.system.F.T @ P @ self.system.G).T
+            @ np.linalg.inv(self.R + self.system.G.T @ P @ self.system.G)
+            @ (self.system.F.T @ P @ self.system.G)
+        )
+
+    def _gain(self, P: np.ndarray) -> np.ndarray:
+        return (
+            -np.linalg.inv(self.R + self.system.G.T @ P @ self.system.G)
+            @ self.system.G.T
+            @ P
+            @ self.system.F
+        )
+
+    def initialize_gain_matrices(self):
+        P_series = np.zeros((self.num_steps, 2, 2))
+        P_series[-1] = self.M
+
+        for i in range(2, self.num_steps + 1):
+            P_series[-i] = self._riccati(P_series[-(i - 1)])
+
+        self.K_series = np.zeros((self.num_steps - 1, 1, 2))
+        for i in range(self.num_steps - 1):
+            self.K_series[i] = self._gain(P_series[i])
+
+        v_series = np.zeros((self.num_steps, 2, 1))
+        v_series[-1] = self.Q @ self.r_series[-1]
+        for i in range(2, self.num_steps + 1):
+            v_series[-i] = (
+                self.system.F - self.system.G @ self.K_series[-i]
+            ).T @ v_series[-(i - 1)] + self.Q @ self.r_series[-i]
+
+        self.Kov_series = np.zeros((self.num_steps - 1))
+        for i in range(len(self.Kov_series)):
+            Ko = (
+                np.linalg.inv(
+                    self.R + self.system.G.T @ P_series[i + 1] @ self.system.G
+                )
+                @ self.system.G.T
+            )
+            self.Kov_series[i] = Ko @ v_series[i + 1]
+
+    def _measure(self) -> tuple[np.ndarray, np.ndarray]:
+        x_actual = self.system.get_actual()
+        self.estimator.measurement_update(self.system.measure())
         x_hat = self.estimator.get_estimate()
-        ...
+        return x_actual, x_hat
+
+    def _step(self, x_hat: np.ndarray, i: int):
+        # estimate the system state
+        u_star = -self.K_series[i] @ x_hat + self.Kov_series
+        self.system.step(u_star)
+        self.estimator.dynamics_update(u_star)
+
+    def _measure_and_step(self, i: int) -> tuple[np.ndarray, np.ndarray]:
+        x_actual, x_hat = self._measure()
+        self._step(x_hat, i)
+        return x_actual, x_hat
+
+    def get_traces(self) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        x_trace = np.zeros((self.num_steps, 2, 1))
+        x_hat_trace = np.zeros((self.num_steps, 2, 1))
+
+        for i in range(self.num_steps - 1):
+            x_trace[i], x_hat_trace[i] = self._measure_and_step(i)
+        x_trace[-1], x_hat_trace[-1] = self._measure()
+
+        pos_trace = x_trace[:, 0]
+        v_trace = x_trace[:, 1]
+
+        pos_hat_trace = x_hat_trace[:, 0]
+        v_hat_trace = x_hat_trace[:, 1]
+        return pos_trace, v_trace, pos_hat_trace, v_hat_trace
--- LQG-tracker/dynamics.py
+++ LQG-tracker/dynamics.py
@@ -1,5 +1,4 @@
 import numpy as np
-from dataclasses import dataclass
 
 
 class NoisyDoubleIntegrator:
@@ -22,14 +21,22 @@ class NoisyDoubleIntegrator:
 
     x: np.ndarray
 
-    def __init__(self, sigma_wx2: float, sigma_wv2: float, sigma_eta2: float, mu_x0: float,
-                 sigma_x02: float, mu_v0: float, sigma_v02: float):
+    def __init__(
+        self,
+        sigma_wx2: float,
+        sigma_wv2: float,
+        sigma_eta2: float,
+        mu_x0: float,
+        sigma_x02: float,
+        mu_v0: float,
+        sigma_v02: float,
+    ):
         """
         Initialize parameters and the initial state according to the Gaussian distribution specified
         by the input parameters.
         """
-        self.sigma_wx2 = sigma_wx2,
-        self.sigma_wv2 = sigma_wv2,
+        self.sigma_wx2 = (sigma_wx2,)
+        self.sigma_wv2 = (sigma_wv2,)
         self.sigma_eta2 = sigma_eta2
         self.mu_x0 = mu_x0
         self.sigma_x02 = sigma_x02
@@ -51,3 +58,6 @@ class NoisyDoubleIntegrator:
     def measure(self) -> np.ndarray:
         eta = np.random.normal(scale=np.sqrt(self.sigma_eta2))
         return self.H @ self.x + eta
+
+    def get_actual(self) -> np.ndarray:
+        return self.x
